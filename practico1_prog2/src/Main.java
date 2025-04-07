4c2cdad1f74cd405b3a860b610ee9ad36e7271ad
valeartola <mv.artola@alumno.um.edu.ar>
2025-04-07 16:20:48 -0300

    creación clase vehiculoprinter y modificación de  main

--- practico1_prog2/src/Main.java
+++ practico1_prog2/src/Main.java
@@ -1,15 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        List<Vehiculo> vehiculos = new ArrayList<>();
 
-        Vehiculo t = new Vehiculo("AB123CD", "Toyota", 2020, 1000);
-        t.mostrarInformacion();
-        Vehiculo p = new Vehiculo("AC857DF", "Peugeot", 2023, 1000);
-        p.mostrarInformacion();
-        Vehiculo r = new Vehiculo("AA859HI", "Renault", 2018, 1100);
-        r.mostrarInformacion();
-        Vehiculo c = new Vehiculo("AF378VA", "Citroen", 2024, 1500);
-        c.mostrarInformacion();
+        vehiculos.add(new Vehiculo("AB123CD", "Toyota", 2020, 1000));
+        vehiculos.add(new Vehiculo("AC857DF", "Peugeot", 2023, 1000));
+        vehiculos.add(new Vehiculo("AA859HT", "Renault", 2018, 1100));
+        vehiculos.add(new Vehiculo("AF378VA", "Citroen", 2024, 1500));
+
+        VehiculoPrinter printer = new VehiculoPrinter();
+
+        for (Vehiculo v : vehiculos) {
+            printer.imprimir(v);
+        }
     }
 }
+
+
