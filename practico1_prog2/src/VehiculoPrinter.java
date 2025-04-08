4cf792657e242e007c477c388b0cfcb4534e4418
valeartola <mv.artola@alumno.um.edu.ar>
2025-04-08 11:23:51 -0300

    polimorfismo subclase camion

--- practico1_prog2/src/VehiculoPrinter.java
+++ practico1_prog2/src/VehiculoPrinter.java
@@ -1,18 +1,8 @@
 public class VehiculoPrinter {
     public void imprimir(Vehiculo vehiculo) {
-        System.out.print("Vehículo: " + vehiculo.getPatente() + " " + vehiculo.getMarca() + " " + vehiculo.getAnio() + " " + vehiculo.getCapacidadCargaKg());
-
-        if (vehiculo instanceof Camion) {
-            Camion camion = (Camion) vehiculo;
-            if (camion.isTieneAcoplado()) {
-                System.out.print(" | Tiene acoplado");
-            } else {
-                System.out.print(" | Sin acoplado");
-            }
-        }
-
-        System.out.println();
+        System.out.println(vehiculo.getDescripcion());
     }
 }
+
 
 
